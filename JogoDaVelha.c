6acb66facea16fcc4e5beb8c31edfe2d223eb26f
Kelly Almeida <kellydealmeidasantos237@gmail.com>
2026-04-12 14:46:21 -0300

    Refactor JogoDaVelha for player names and dual mode

--- JogoDaVelha.c
+++ JogoDaVelha.c
@@ -4,6 +4,7 @@
 #include <time.h>
 #include<stdbool.h>
 #include<string.h>
+#include <unistd.h>
 
 //Declaração de funções
 
@@ -12,81 +13,140 @@ void jogadaComp(char tab[3][3]);
 bool analise(char tab[3][3], int coordenadas[]);  
 
     //Funcionamento universal
-    
-/*bool modoJogo();*/
-bool verificaP(char tab[3][3], bool comp, int numJogada);
+void limparTela();  
+bool verificaP(char tab[3][3], bool comp, int numJogada, bool modoDuo, char nomePlayer[100]);
 bool continuacao();  
 void MostrarTabela(char tab[3][3]);
 
-void jogadaUser(char tab[3][3]); //Incluir o mecânismo do nome do jogador   
-void mensagemFim(bool comp, bool empate); //Incluir mensagens para o modo de dois jogadores
+void jogadaUser(char tab[3][3], bool modoDuo, char player[100], int numJogador); //Incluir o mecânismo do nome do jogador   
+void mensagemFim(bool comp, bool empate, bool modoDuo, char nomePlayer[100]); //Incluir mensagens para o modo de dois jogadores
 void marcar(char tab[3][3], int i, int j, bool comp); //Adicionar uma parte para um jogador secundário
-bool escolValida(char op, char tab[3][3], bool comp); //Adicionar adaptação para segundo jogador
-void placar(); //Adicinar a uma adaptação para o jogadores
+bool escolValida(char op, char tab[3][3], bool bolinha); //Adicionar adaptação para segundo jogador
+void placar(char nomePlayer1[100], char nomePlayer2[100]); //Adicinar a uma adaptação para o jogadores
 
 
     //Modo Jogadores
-/*string nomeJogador (); // adiciona os nomes dos jogadores*/
+void nomePlayer (bool modoDuo, char nome[100],int numPlayer);
+bool modoJogo();
     
 
 //Variáveis globais
 
-int vitoUser;
+int vitoPlayer1;
+int vitoPlayer2;
 int vitoComp;
 int numEmpate;
 int numPartidas;
-/*string nomePlayer1*/
-/*string nomePlayer2*/
-/*bool jodoDuo;*/
+char nomePlayer1 [100];
+char nomePlayer2 [100];
+
 
 
 
 void main(){
     bool jogando = true;
-    vitoUser = 0;
-    vitoComp = 0;
+    vitoPlayer1 = 0;
+    vitoPlayer2 = 0;
     numEmpate = 0;
+    bool modBoot;
     
     //Modo de dois Jogadores...
     
+    modBoot = modoJogo();
     
-    do{
-        //Declarações
-       char tabela [3][3] = {
-            {'1', '2', '3'},
-            {'4', '5', '6'},
-            {'7', '8', '9'}
-        };
-        int numJogadas = 0;
-        bool partida = true;
+    if (modBoot){
+        nomePlayer (false, nomePlayer1, 0);
         
         do{
-           //mostrar tabela do Jogo
-           MostrarTabela(tabela);
-           
-           //pedir jogada do usuário
-           jogadaUser(tabela);
-           numJogadas++;
-           
-           //Verficar se derrota,vitória ou empate
-           partida = verificaP(tabela, false, numJogadas);
-           
-           if (partida){
-               //sortear jogada do computador
-               jogadaComp(tabela);
+            //Declarações
+           char tabela [3][3] = {
+                {'1', '2', '3'},
+                {'4', '5', '6'},
+                {'7', '8', '9'}
+            };
+            int numJogadas = 0;
+            bool partida = true;
+            char nomeComp[100] = "Th3odory_boot";
+            
+            
+            do{
+               //mostrar tabela do Jogo
+               MostrarTabela(tabela);
+               
+               //pedir jogada do usuário
+               jogadaUser(tabela, false, nomePlayer1, 1);
                numJogadas++;
                
                //Verficar se derrota,vitória ou empate
-               partida = verificaP(tabela, true, numJogadas);
-           }
+               partida = verificaP(tabela, false, numJogadas, false, nomePlayer1);
+               
+               if (partida){
+                   //sortear jogada do computador
+                   jogadaComp(tabela);
+                   numJogadas++;
+                   
+                   //Verficar se derrota,vitória ou empate
+                   partida = verificaP(tabela, true, numJogadas, false, nomeComp);
+               }
+               
+            }while(partida);
+            placar(nomePlayer1,nomeComp);
            
-        }while(partida);
-        placar();
-       
-       //pergunta de continuação  
-       jogando = continuacao();
-    }while (jogando);
-
+           //pergunta de continuação  
+           jogando = continuacao();
+        }while (jogando);
+    } else{
+        nomePlayer (true, nomePlayer1, 1);
+        nomePlayer(true, nomePlayer2, 2);
+        
+        do{
+            //Declarações
+           char tabela [3][3] = {
+                {'1', '2', '3'},
+                {'4', '5', '6'},
+                {'7', '8', '9'}
+            };
+            int numJogadas = 0;
+            bool partida = true;
+            
+            
+            do{
+               //mostrar tabela do Jogo
+               MostrarTabela(tabela);
+               
+               //Jogada do primeiro jogador
+               jogadaUser(tabela, true, nomePlayer1, 1);
+               numJogadas++;
+               
+               //Verificação de derrota, empate ou vitória
+               partida = verificaP(tabela, false, numJogadas, true, nomePlayer1);
+               
+               
+               
+               //Jogada do segundo jogador
+               if (partida){
+                   //Mostrar a Tabela
+                    MostrarTabela(tabela);
+                   //sortear jogada do computador
+                   jogadaUser(tabela, true, nomePlayer2, 2);
+                   numJogadas++;
+                   
+                   //Verficar se derrota,vitória ou empate
+                   partida = verificaP(tabela, true, numJogadas, true, nomePlayer2);
+               }
+               
+            }while(partida);
+            //placar
+            placar(nomePlayer1,nomePlayer2);
+            
+           
+           //pergunta de continuação  
+           jogando = continuacao();
+        }while (jogando);
+    }
+    
+    
+    
 }
 
 //Mensagem de continuação
@@ -120,7 +180,7 @@ bool continuacao (){
 
 //Mostra a tabela do jogo
 void MostrarTabela(char tab[3][3]){
-    system("clear");
+    limparTela();
     
     printf("\n=================\n");
     printf("\n  Jogo da velha\n");
@@ -142,20 +202,43 @@ void MostrarTabela(char tab[3][3]){
 }
 
 // Marca a resposta do usuário
-void jogadaUser(char tab[3][3]){
+void jogadaUser(char tab[3][3], bool modoDuo, char player[100], int numJogador){
     bool escolhendo = true;
+    bool bolinha;
     char escol;
-    do{
-        printf("\n=================\n");
-        printf("Escolha uma posição: ");
-        scanf(" %c", &escol);
-        
-        if (escolValida(escol, tab, false)){
-            escolhendo = false;
-        }else{
-            printf("Posição inválida!!");
-        }
-    }while(escolhendo);
+    
+    if(numJogador == 1){
+        bolinha = false;
+    } else{
+        bolinha = true;
+    }
+    
+    if(modoDuo){
+       do{
+            printf("\n=================\n");
+            printf("%s, escolha uma posição: ", player);
+            scanf(" %c", &escol);
+            
+            if (escolValida(escol, tab, bolinha)){
+                escolhendo = false;
+            }else{
+                printf("Posição inválida!!");
+            }
+       }while(escolhendo);
+    } else{
+        do{
+            printf("\n=================\n");
+            printf("Escolha uma posição: ");
+            scanf(" %c", &escol);
+            
+            if (escolValida(escol, tab, false)){
+                escolhendo = false;
+            }else{
+                printf("Posição inválida!!");
+            }
+        }while(escolhendo);
+    }
+    
 }
 
 // Escolhe a posição do boot
@@ -271,13 +354,13 @@ bool analise(char tab [3][3], int coordenadas[2]){
 }
 
 //Verificação de se a posição escolhida é válida
-bool escolValida(char op, char tab[3][3], bool comp){
+bool escolValida(char op, char tab[3][3], bool bolinha){
     
     for(int i = 0; i < 3; i++){
         for(int j = 0; j < 3; j++){
             
             if (op == tab[i][j] && op != 'X' && op != 'O'){
-                marcar(tab, i, j, comp);
+                marcar(tab, i, j, bolinha);
                 return true;
             }
         }
@@ -295,7 +378,7 @@ void marcar (char tab[3][3], int i, int j, bool comp){
 }
 
 //Verificação de vitória, derrota ou empate
-bool verificaP(char tab[3][3], bool comp, int numJogadas){
+bool verificaP(char tab[3][3], bool comp, int numJogadas, bool modoDuo, char nomePlayer[100]){
     bool resp = true;
     bool empate = false;
   
@@ -331,46 +414,106 @@ bool verificaP(char tab[3][3], bool comp, int numJogadas){
     //Verificação de empate
     
     if (numJogadas == 9 && resp){
+        MostrarTabela(tab);
         resp = false;
         empate = true;
     } 
-    
+
     
     //Acionamento de mensagem 
     if(!resp){
-        mensagemFim(comp, empate);
-    }
+        mensagemFim(comp, empate, modoDuo, nomePlayer);
+    } 
     
     return resp;
 }
 
 //Mensagens de final de partida
-void mensagemFim(bool comp, bool empate){
-    printf("\nFIM DE JOGO!!\n");
+void mensagemFim(bool bolinha, bool empate, bool modoDuo, char nomePlayer[100]){
+    printf("\nFIM DE JOGO!!\n\n");
+    sleep(1);
     
     if (!empate){
-        if(comp){
-            printf("Vitória do computador!!\n");
-            vitoComp++;
+            if(bolinha){
+                printf("Vitória de %s!!\n", nomePlayer);
+                vitoPlayer2++;
+            }else{
+                printf("Vitória de %s!!\n", nomePlayer);
+                vitoPlayer1++;
+            }
         }else{
-            printf("Vitória do jogador!!\n");
-            vitoUser++;
+            printf("Empate\n");
+            numEmpate++;
         }
-    }else{
-        printf("Empate\n");
-        numEmpate++;
-    }
+        
     numPartidas++;
+    sleep(1);
+    
 }
 
 //Placar
-void placar(){
+void placar(char nomePlayer1[100],char nomePlayer2[100]){
     printf("\n==============\n");
     printf("    Placar");
     printf("\n==============\n");
     printf("Número de partidas: %d \n", numPartidas);
-    printf("User: %d \n", vitoUser);
-    printf("Computador: %d \n", vitoComp);
+    printf("%s: %d \n", nomePlayer1,vitoPlayer1);
+    printf("%s: %d \n",nomePlayer2, vitoPlayer2 );
     printf("Empates: %d \n", numEmpate);
     printf("\n==============\n");
+}
+
+//Escolher modo do jogo
+
+//Declaração de nome de player
+void nomePlayer (bool modoDuo, char nome[100], int numPlayer){
+    
+    
+    printf("\n=*=*=*=*=*=*=*=*=*=*=*\n");
+    
+    if (modoDuo){
+        printf("Digite o nome do %dº jogador: ", numPlayer);
+        scanf("%s", nome);
+    }else{
+        printf("Digite seu nome: ");
+        scanf("%s", nome);
+    }
+    
+  
+}
+
+bool modoJogo(){
+    bool resp = true;
+    int escol = 0;
+    
+    do{
+        printf("\n=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=\n");
+        printf("\tSelecione o modo de jogo\n");
+        printf("\n=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=\n");
+        printf("\t[1] - Jogador e boot\n");
+        printf("\t[2] - Dois jogadores\n");
+        
+        printf("==> ");
+        scanf("%d", &escol);
+        
+        if(escol == 1){
+            resp = true;
+        }else if(escol == 2){
+            resp = false;
+        } else{
+            printf("Escolha um valor válido!!\n");
+        }
+      
+    }while(escol != 1 && escol != 2);
+    
+      
+    return resp;
+}
+
+void limparTela() {
+    #ifdef _WIN32
+        system("cls");
+    #else
+        system("clear");
+    #endif
 }
